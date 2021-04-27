c1652bcba276611d4187373b3a86096fde4e998c
Paul J. Shaw <pavlvsxavier@gmail.com>
2021-04-27 01:13:25 +0100

    wip

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -3,6 +3,16 @@ import icons from 'url:../../img/icons.svg'
 export default class View {
     _data
 
+
+    /**
+     * Render the received object to the DOM
+     * @param  {Object | Object[]} data The data to be rendered (e.g. a recipe)
+     * @param  {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+     * @returns {undefined | string} Returns a markup string if render is false
+     * @this {Object} View instance
+     * @author pavlvs - paul@canarius.co.uk
+     * @todo Finish implementation
+     */
     render(data, render = true) {
         if (!data || (Array.isArray(data) && data.length === 0)) return this.renderError()
 
